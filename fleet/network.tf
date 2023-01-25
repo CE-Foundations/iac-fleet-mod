6c37039eed16c39e651c98661c6c4d0922856644
Luis Gonzalez <luisegr149@gmail.com>
2023-01-25 08:47:46 -0600

    Update locals org.tf

--- fleet/network.tf
+++ fleet/network.tf
@@ -58,17 +58,6 @@ resource "google_compute_external_vpn_gateway" "peer" {
   ]
 }
 
-locals {
-  fleet_vpn_peer_config_info = flatten([
-    for project in module.control_plane_networking_project : [
-      for cluster in var.fleet_vpn_peer_config :
-      merge(cluster, { project_id = project.project_id })
-      if cluster.fleet == trimsuffix(substr(project.project_id, 0, length(project.project_id) - 5), var.suffix.network_project)
-    ]
-  ])
-
-}
-
 
 # creates and attaches tunnels for each cluster
 module "tunnels" {
--- fleet/org.tf
+++ fleet/org.tf
@@ -102,6 +102,17 @@ module "control_plane_networking_project" {
   ]
 }
 
+locals {
+  fleet_vpn_peer_config_info = flatten([
+    for project in module.control_plane_networking_project : [
+      for cluster in var.fleet_vpn_peer_config :
+      merge(cluster, { project_id = project.project_id })
+      if cluster.fleet == trimsuffix(substr(project.project_id, 0, length(project.project_id) - 5), var.suffix.network_project)
+    ]
+  ])
+}
+
+
 module "control_plane_sds_project" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 10.1"
