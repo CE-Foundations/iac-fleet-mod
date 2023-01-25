e1c65481d07a435499ad813db65e06b6f1e2da8f
Luis Gonzalez <luisegr149@gmail.com>
2023-01-25 08:44:32 -0600

    Testing locals order

--- fleet/network.tf
+++ fleet/network.tf
@@ -58,6 +58,18 @@ resource "google_compute_external_vpn_gateway" "peer" {
   ]
 }
 
+locals {
+  fleet_vpn_peer_config_info = flatten([
+    for project in module.control_plane_networking_project : [
+      for cluster in var.fleet_vpn_peer_config :
+      merge(cluster, { project_id = project.project_id })
+      if cluster.fleet == trimsuffix(substr(project.project_id, 0, length(project.project_id) - 5), var.suffix.network_project)
+    ]
+  ])
+
+}
+
+
 # creates and attaches tunnels for each cluster
 module "tunnels" {
   source = "./submodules/tunnels"
@@ -75,6 +87,13 @@ module "tunnels" {
   depends_on = [
     module.control_plane_networking_project
   ]
+}
+
+
+locals {
+  tunnels_info = {
+    for tunnel in module.tunnels : tunnel.cluster => tunnel
+  }
 }
 
 
@@ -98,16 +117,3 @@ module "bgp" {
 
 
 
-locals {
-  fleet_vpn_peer_config_info = flatten([
-    for project in module.control_plane_networking_project : [
-      for cluster in var.fleet_vpn_peer_config :
-      merge(cluster, { project_id = project.project_id })
-      if cluster.fleet == trimsuffix(substr(project.project_id, 0, length(project.project_id) - 5), var.suffix.network_project)
-    ]
-  ])
-
-  tunnels_info = {
-    for tunnel in module.tunnels : tunnel.cluster => tunnel
-  }
-}
